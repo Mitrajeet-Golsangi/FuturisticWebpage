15411607319cfbbfe506e0880073f997b23d3e37
Mitrajeet Golsangi <mitrajeetgolsangi@gmail.com>
2021-09-26 14:10:15 +0530

    banner modifications and fade ins

--- static/js/fadeIns.js
+++ static/js/fadeIns.js
@@ -3,14 +3,22 @@ const appearOptions = {
     threshold: 0,
     rootMargin: "0% 0% -30% 0%"
 }
-
 const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
     entries.forEach(entry => {
+
+        let audio = entry.target.querySelectorAll('audio')[0];
+
         // Checking if the entry is intersecting don't toggle class
+
         if (!entry.isIntersecting) {
             return;
         } else {
             entry.target.classList.add('appear');
+            console.log(audio);
+            // audio.play();
+            setTimeout(() => {
+                document.getElementsByTagName("img")[0].style.opacity = 0;
+            }, 2000);
             appearOnScroll.unobserve(entry.target);
         }
     })
@@ -20,8 +28,3 @@ const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
 sliders.forEach(slider => {
     appearOnScroll.observe(slider);
 })
-
-// Audio effects
-const audio = document.getElementById("fade-audio-top-1");
-
-document.getElementById("fade-img-top-1").onmouseover = () => audio.play();
